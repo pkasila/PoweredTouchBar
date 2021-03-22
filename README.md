3275b19bbadc50040b12433431984672a058259c
Pavel Kasila <pavel.kasila@gmail.com>
2021-03-22 16:30:54 +0300

    Simple README.md, update PoweredTouchBarButton, so it can be a target for NSButtonTouchBarItem, so objAction will be called

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # PoweredTouchBar
 
-A description of this package.
+PoweredTouchBar is a library which lets you easily and more powerfully use Touch Bar on macOS with SwiftUI.
+
+## How to use it?
+There is a simple example of how to use `PoweredTouchBar` library:
+```swift
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TextEditor(text: $text)
+            .poweredTouchBar {
+                PoweredTouchBarButton(
+                    identifier: "HelloWorld",
+                    title: "Hello World!",
+                    action: {
+                        print("Yeah, it works!")
+                    }
+                )
+            }
+    }
+}
+```
--- Sources/PoweredTouchBar/PoweredTouchBarItems/PoweredTouchBarButton.swift
+++ Sources/PoweredTouchBar/PoweredTouchBarItems/PoweredTouchBarButton.swift
@@ -7,7 +7,7 @@
 
 import AppKit
 
-public class PoweredTouchBarButton: PoweredTouchBarItem {
+public class PoweredTouchBarButton: NSObject, PoweredTouchBarItem {
     
     var identifier: String
     var item: NSButtonTouchBarItem
@@ -15,19 +15,19 @@ public class PoweredTouchBarButton: PoweredTouchBarItem {
     
     public init(identifier: String, title: String, action: @escaping () -> Void) {
         self.identifier = identifier
-        self.item = NSButtonTouchBarItem(identifier: NSTouchBarItem.Identifier(self.identifier), title: title, target: nil, action: #selector(objAction))
+        self.item = NSButtonTouchBarItem(identifier: NSTouchBarItem.Identifier(self.identifier), title: title, target: self, action: #selector(objAction))
         self.action = action
     }
     
     public init(identifier: String, image: NSImage, action: @escaping () -> Void) {
         self.identifier = identifier
-        self.item = NSButtonTouchBarItem(identifier: NSTouchBarItem.Identifier(self.identifier), image: image, target: nil, action: #selector(objAction))
+        self.item = NSButtonTouchBarItem(identifier: NSTouchBarItem.Identifier(self.identifier), image: image, target: self, action: #selector(objAction))
         self.action = action
     }
     
     public init(identifier: String, title: String, image: NSImage, action: @escaping () -> Void) {
         self.identifier = identifier
-        self.item = NSButtonTouchBarItem(identifier: NSTouchBarItem.Identifier(self.identifier), title: title, image: image, target: nil, action: #selector(objAction))
+        self.item = NSButtonTouchBarItem(identifier: NSTouchBarItem.Identifier(self.identifier), title: title, image: image, target: self, action: #selector(objAction))
         self.action = action
     }
     
@@ -39,7 +39,7 @@ public class PoweredTouchBarButton: PoweredTouchBarItem {
         return NSTouchBarItem.Identifier(identifier)
     }
     
-    @objc func objAction() {
+    @objc func objAction(_ sender: Any) {
         action()
     }
 }
