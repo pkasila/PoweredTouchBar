b9d0179969f2675f774fe8faa967f10428e3107c
Pavel Kasila <pavel.kasila@gmail.com>
2021-03-22 14:49:55 +0300

    Add TouchBarContainedViewController

--- Sources/PoweredTouchBar/PoweredTouchBar.swift
+++ Sources/PoweredTouchBar/PoweredTouchBar.swift
@@ -1,3 +1,26 @@
-struct PoweredTouchBar {
-    var text = "Hello, World!"
+//
+//  PoweredTouchBar.swift
+//
+//
+//  Created by Pavel Kasila on 22.03.21.
+//
+
+import SwiftUI
+
+public struct PoweredTouchBar<Content: View>: NSViewControllerRepresentable {
+    var content: () -> Content
+    
+    public init(content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    public func makeNSViewController(context: Context) -> TouchBarContainedViewController<Content> {
+        let vc = TouchBarContainedViewController<Content>()
+        vc.content = content
+        return vc
+    }
+    
+    public func updateNSViewController(_ nsViewController: TouchBarContainedViewController<Content>, context: Context) {
+        
+    }
 }
---
+++ Sources/PoweredTouchBar/TouchBarContainedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TouchBarContainedViewController.swift
+//  
+//
+//  Created by Pavel Kasila on 22.03.21.
+//
+
+import AppKit
+import SwiftUI
+
+public class TouchBarContainedViewController<Content: View>: NSViewController {
+    var content: (() -> Content)!
+    
+    public override func loadView() {
+        self.view = NSHostingView(rootView: content())
+        super.loadView()
+    }
+}
